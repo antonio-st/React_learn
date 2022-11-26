dbed5f927df204ef4889e8b8da9a5ed00179e863
antonio-st <antonio-st@yandex.ru>
2022-11-26 23:12:45 +0300

    add product cards

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import cartWhite from './assets/img/cartWhite.svg';
+import rating from './assets/img/rating.svg';
+
 export const Product = () => {
+
+    const [product, setProduct] = useState(null)
+
+
+    useEffect(() => {
+        let promise = axios.get('https://masterclass.kimitsu.it-incubator.ru/api/products/2')
+        promise.then((res) => {
+            setProduct(res.data)
+        })
+    }, [])
+
     return (
-        <h1>Product</h1>
+        <div>
+            <div>Заглушка. Понадобится чуть позже. Не удаляейте :)</div>
+
+            {
+                product === null
+                    ? <h1> Loading... </h1>
+                    : <div className="product">
+                        <img src={product.image} alt="" />
+                        <div className="info">
+                            <p className="title">{product.title}</p>
+                            <p className="price">$ {product.price}</p>
+                            <div className="rating">
+                                <p>Rating: {product.rating.rate}</p>
+                                <img src={rating} alt="" />
+                            </div>
+                            <div className="category">
+                                <span>Category:</span>
+                                <p>{product.category}</p>
+                            </div>
+                            <p className="description">{product.description}</p>
+                            <button>
+                                <img src={cartWhite} alt="" />
+                                Add to cart
+                            </button>
+                        </div>
+                    </div>
+            }
+        </div>
+
     )
 }
